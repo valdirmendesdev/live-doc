c37746d36fbddcf1317895a868b280f2b9c508ca
Valdir Mendes <valdir.mfjesus@hotmail.com>
2021-08-12 00:14:32 -0300

    feat: Adding logger middleware

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New())
 
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.JSON(&fiber.Map{
