1fabd5d6d801a54d7f64e9c2e0544105ac4760b7
Valdir Mendes <valdir.mfjesus@hotmail.com>
2021-09-14 00:22:38 -0300

    refactor: customer handler tests

--- internal/http/rest/controllers/customers_test.go
+++ internal/http/rest/controllers/customers_test.go
@@ -29,66 +29,72 @@ func createCustomerController(t *testing.T) (*mocks.MockCustomer, controllers.Cu
 	return r, c
 }
 
-func Test_CustomerFindByID_WithInvalidUUID(t *testing.T) {
-	_, cc := createCustomerController(t)
-	app := fiber.New()
-	app.Get("/:id", cc.FindById)
-
-	req, err := http.NewRequest("GET", "/abcd", nil)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	res, err := app.Test(req)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
-	assert.Equal(t, string(body), `{"error":"invalid UUID length: 4"}`)
-}
-
-func Test_CustomerFindByID_CustomerNotFound(t *testing.T) {
-	r, cc := createCustomerController(t)
-	app := fiber.New()
-	app.Get("/:id", cc.FindById)
-
-	id := types.NewID()
-	r.EXPECT().
-		GetById(id).
-		Return(nil, gorm.ErrRecordNotFound)
-
-	req, err := http.NewRequest("GET", "/"+id.String(), nil)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	res, err := app.Test(req)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-
-	assert.Equal(t, http.StatusNotFound, res.StatusCode)
-	assert.Equal(t, string(body), `{"error":"record not found"}`)
-}
-
 func Test_CustomerFindByID(t *testing.T) {
+	tests := []struct {
+		name       string
+		id         string
+		error      error
+		statusCode int
+		body       string
+	}{
+		{
+			name:       "Invalid UUID",
+			id:         "abcd",
+			error:      nil,
+			statusCode: http.StatusBadRequest,
+			body:       `{"error":"invalid UUID length: 4"}`,
+		},
+		{
+			name:       "Customer Not Found",
+			id:         types.NewID().String(),
+			error:      gorm.ErrRecordNotFound,
+			statusCode: http.StatusNotFound,
+			body:       `{"error":"record not found"}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r, cc := createCustomerController(t)
+			app := fiber.New()
+			app.Get("/:id", cc.FindById)
+
+			if test.error != nil {
+				id, err :=  types.ParseID(test.id)
+				if err != nil {
+					t.Log(err)
+					return
+				}
+				r.EXPECT().
+					GetById(id).
+					Return(nil, test.error)
+			}
+
+			req, err := http.NewRequest("GET", "/"+test.id, nil)
+			if err != nil {
+				t.Log(err)
+				return
+			}
+
+			res, err := app.Test(req)
+			if err != nil {
+				t.Log(err)
+				return
+			}
+
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Log(err)
+				return
+			}
+
+			assert.Equal(t, test.statusCode, res.StatusCode)
+			assert.Equal(t, string(body), test.body)
+		})
+	}
+}
+
+func Test_CustomerFindByID_Success(t *testing.T) {
 	r, cc := createCustomerController(t)
 	app := fiber.New()
 	app.Get("/:id", cc.FindById)
