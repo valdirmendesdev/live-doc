d05e855263621cea2a9f4ed2788f7540ac042810
Valdir Mendes <valdir.mfjesus@hotmail.com>
2021-09-26 00:47:36 -0300

    refactor: Adjusting app structure to test

--- cmd/server.go
+++ cmd/server.go
@@ -4,13 +4,20 @@ import (
 	"log"
 
 	_ "github.com/lib/pq"
+	"github.com/valdirmendesdev/live-doc/internal/config"
 	"github.com/valdirmendesdev/live-doc/internal/http/rest"
+	"github.com/valdirmendesdev/live-doc/internal/storage/postgres"
 )
 
 func main() {
+	config.InitDatabase()
+
+	cr := postgres.NewCustomerRepository()
 
 	app := rest.NewApp()
-	app.Setup()
+	app.HealthRoutes()
+	app.CustomersRoutes(&cr)
+
 	err := app.Serve()
 	if err != nil {
 		log.Fatal(err)
--- internal/http/rest/app.go
+++ internal/http/rest/app.go
@@ -3,43 +3,38 @@ package rest
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/valdirmendesdev/live-doc/internal/config"
+	"github.com/gofiber/helmet/v2"
 	"github.com/valdirmendesdev/live-doc/internal/http/rest/controllers"
-	"github.com/valdirmendesdev/live-doc/internal/storage/postgres"
+	"github.com/valdirmendesdev/live-doc/internal/live-docs/core/customer"
 )
 
 type App struct {
-	router *fiber.App
+	Router *fiber.App
 }
 
 func NewApp() App {
-	return App{
-		router: fiber.New(),
+	a := App{
+		Router: fiber.New(),
 	}
+	a.Router.Use(logger.New())
+	a.Router.Use(helmet.New())
+	return a
 }
 
-func (a *App) healthRoutes() {
-	g := a.router.Group("/health")
+func (a *App) HealthRoutes() {
+	g := a.Router.Group("/health")
 	c := controllers.NewHealth()
 	g.Get("/", c.Status)
 }
 
-func (a *App) customersRoutes() error {
-	r := postgres.NewCustomerRepository()
-	c := controllers.NewCustomer(&r)
-	g := a.router.Group("/customers")
+func (a *App) CustomersRoutes(r customer.Repository) {
+	c := controllers.NewCustomer(r)
+	g := a.Router.Group("/customers")
 	g.Get("/", c.ListAll)
 	g.Get("/:id", c.FindById)
-	return nil
-}
-
-func (a *App) Setup() {
-	config.InitDatabase()
-	a.router.Use(logger.New())
-	a.healthRoutes()
-	a.customersRoutes()
+	g.Post("/", c.Create)
 }
 
 func (a *App) Serve() error {
-	return a.router.Listen(":8080")
+	return a.Router.Listen(":8080")
 }
